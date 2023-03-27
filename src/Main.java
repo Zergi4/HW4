b14e155a6c577d05eb9bbc3436da93fad3d07cad
Zergi4 <ksyssau@ya.ru>
2023-03-27 20:06:08 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 10; //hw4.1
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " , то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " , то он несовершеннолетний");
+        }
+        int temp = -5; //hw4.2
+        if (temp <= 5) {
+            System.out.println(" На улице " + temp + " градусов, нужно одеть шапку");
+        } else {
+            System.out.println(" На улице " + temp + " градусов, не нужно одевать шапку");
+        }
 
-            }
+
+    }
 }
